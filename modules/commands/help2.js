2c1cfa784468353653cb9deeb37e0c169747b299
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-05-05 10:25:50 +0600

    Update help2.js

--- modules/commands/help2.js
+++ modules/commands/help2.js
@@ -1,1 +1,48 @@
+module.exports.config = {
+ name: "help2",
+ version: "2.0.0",
+ hasPermssion: 0,
+ credits: "BADOL-KHAN",//You Can Change The Gif Or Img If You Want Just Put The Link Of Your Gif Or Img To The Var Link
+ description: "commands list",
+ commandCategory: "system",
+ usages: "module name",
+ cooldowns: 1,
+ envConfig: {
+  autoUnsend: false,
+  delayUnsend: 300
+ }
+};
 
+module.exports.languages = {
+ "en": {
+  "moduleInfo": "─────[ %1 ]──────\n\nUsage: %3\nCategory: %4\nWaiting time: %5 seconds(s)\nPermission: %6\nDescription: %2\n\nModule coded by %7",
+  "helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! ]',
+  "user": "User",
+        "adminGroup": "Admin group",
+        "adminBot": "Admin bot"
+ }
+};
+
+module.exports.handleEvent = function ({ api, event, getText }) {
+ const { commands } = global.client;
+ const { threadID, messageID, body } = event;
+
+ if (!body || typeof body == "undefined" || body.indexOf("help") != 0) return;
+ const splitBody = body.slice(body.indexOf("help")).trim().split(/\s+/);
+ if (splitBody.length == 1 || !commands.has(splitBody[1].toLowerCase())) return;
+ const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+ const command = commands.get(splitBody[1].toLowerCase());
+ const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
+ return api.sendMessage(getText("moduleInfo", command.config.name, command.config.description, `${prefix}${command.config.name} ${(command.config.usages) ? command.config.usages : ""}`, command.config.commandCategory, command.config.cooldowns, ((command.config.hasPermssion == 0) ? getText("user") : (command.config.hasPermssion == 1) ? getText("adminGroup") : getText("adminBot")), command.config.credits), threadID, messageID);
+}
+
+module.exports. run = function({ api, event, args, getText }) {
+  const axios = require("axios");
+  const request = require('request');
+  const fs = require("fs-extra");
+ const { commands } = global.client;
+ const { threadID, messageID } = event;
+ const command = commands.get((args[0] || "").toLowerCase());
+ const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+ const { autoUnsend, delayUnsend } = global.configModule[this.config.name];
+ const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
