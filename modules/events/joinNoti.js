b1d6123f92eba62cf1eb1b8b2382c12588461294
R4K1B-BAHI-009 <123975261+RAKIB-BAHI-009@users.noreply.github.com>
2024-05-25 23:42:48 +0600

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -1,69 +1,76 @@
 module.exports.config = {
-  name: "joinNoti",
-  eventType: ["log:subscribe"],
-  version: "1.0.1",
-  credits: "Rasel",
-  description: "Notify bots or people entering the group",
-  dependencies: {
-    "fs-extra": ""
-  }
-};
-module.exports.run = async function({ api, event }) {
-
-  const request = require("request");
-  const { threadID } = event;
-  if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-    api.changeNickname(`🍒💜𝐑𝐁💙𝐁𝐀𝐃𝐎𝐋💛𝐁𝐎𝐓❤️🍒`, threadID, api.getCurrentUserID());
-    return api.sendMessage(`🍒═══════❤️💛💙💜💚═══════🍒\n\n🤍আসসালামুআলাইকুম-কেমন-আছেন-সবাই🤍\n
-💜বট সংযুক্ত গ্রুপ চ্যাটে সফলভাবে যোগ করা হচ্ছে💙\n
-💛কিরে বাঁদর গুলা শয়তানি করার জন্য অ্যাড দিছস❤️\nযেকোনো কমান্ড দেখতে ${global.config.PREFIX}help ব্যবহার করুন\n
-\n\n╔═══════☆♡☆═══════╗\n\n╰┈➤উদাহারণ➤\n\n╰┈➤${global.config.PREFIX}inf ➤ \n╰┈➤${global.config.PREFIX}admin ➤ \n╰┈➤${global.config.PREFIX}owner ➤ \n╰┈➤${global.config.PREFIX}help ➤\n\n┗━━━━ ✦❘༻༺❘✦ ━━━━━┛\n★★★▰▱▰▱▰▱▰▰▱▰▱▰▱▰▱▰▱▰★★★\n
-★যেকোনো অভিযোগ অথবা হেল্প এর জন্য আমার★\n★বস ✰𝐑𝐁-𝐁𝐀𝐃𝐎𝐋-𝐊𝐇𝐀𝐍 কে নক করতে পারেন★\n\n\n╰┈➤𝐖𝐩: wa.me/+8801782721761\n\n╰┈➤ 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/www.xxx.com61\n\n╰┈➤𝐌𝐄𝐒𝐒𝐄𝐍𝐆𝐀𝐑𝐄: m.me/100000939642985\n\n🍒═══════❤️💛💙💜💚═══════🍒`, threadID);
-  }
-  else {
-    try {
-    const request = require("request");
-      const fs = global.nodemodule["fs-extra"];
-      let { threadName, participantIDs } = await api.getThreadInfo(threadID);
-
-      const threadData = global.data.threadData.get(parseInt(threadID)) || {};
-
-      var mentions = [], nameArray = [], memLength = [], i = 0;
-
-    let addedParticipants1 = event.logMessageData.addedParticipants;
-        for (let newParticipant of addedParticipants1) {
-   let userID = newParticipant.userFbId
-api.getUserInfo(parseInt(userID), (err, data) => {
-      if(err){ return console.log(err)}
-     var obj = Object.keys(data);
-    var userName = data[obj].name.replace("@", "");     	if (userID !== api.getCurrentUserID()) {  
-
-        nameArray.push(userName);
-        mentions.push({ tag: userName, id: userID, fromIndex: 0 });
-
-        memLength.push(participantIDs.length - i++);
-memLength.sort((a, b) => a - b);
-
-      (typeof threadData.customJoin == "undefined") ? msg = "🍒═══════❤️💛💙💜💚═══════🍒\n\n╔════•|      ✿      |•════╗\n 💐আ্ঁস্ঁসা্ঁলা্ঁমু্ঁ💚আ্ঁলা্ঁই্ঁকু্ঁম্ঁ💐\n╚════•|      ✿      |•════╝\n\n    ╰┈➤W⃠E⃠L⃠L⃠ C⃠O⃠M⃠E⃠➤\n\n                 ╰┈➤𝐍𝐄𝐖➤\n\n        ~🇲‌🇪‌🇲‌🇧‌🇪‌🇷‌~\n\n             ╰┈➤ {name} ➤\n\n༄✺আ্ঁপ্ঁনা্ঁকে্ঁ আ্ঁমা্ঁদে্ঁর্ঁ✺࿐\n\n{threadName}\n\n 🌺🌿🌸—এ্ঁর্ঁ প্ঁক্ষ্ঁ🍀থে্ঁকে্ঁ🍀—🌸🌿\n\n         🌿_ভা্ঁলো্ঁবা্ঁসা্ঁ_অ্ঁভি্ঁরা্ঁম্ঁ_🌿\n\n༄✺আঁপঁনিঁ এঁইঁ গ্রুঁপেঁর {soThanhVien} নঁং মে্ঁম্বা্ঁরঁ ࿐\n\n╔═━────━▒•✠•❀•✠• ▒━────━═╗\n╰┈➤𝐁𝐎𝐓'𝐬 𝐎𝐖𝐍𝐄𝐑➤\n\n            ☟                     \n\n💙𝐑𝐁💜𝐁𝐀𝐃𝐎𝐋💛𝐊𝐇𝐀𝐍❤️\n┗━━━━ ✦❘༻•✠•❀•✠•༺❘✦ ━━━━━┛\n🍒═══════❤️💛💙💜💚═══════🍒\n\n▓▓▓▓▓▓▓▓\n\nআমাদের সাথে সময় দেওয়া ও পাশে থাকার অনুরোধ রইলো !!-🍂🌺🥀\n\n🦋༎❤❤༎\n\nⵗⵗ̥̥̊̊ⵗ̥̥̥̥̊̊̊ⵗ̥̥̥̥̥̊̊̊̊ⵗ̥̥̥̥̥̥̊̊̊̊̊ⵗ̥̥̥̥̥̥̥̊̊̊̊̊ⵗ̥̥̥̥̥̥̥̥̊̊̊̊ⵗ̥̥̥̥̥̥̥̥̥̊̊̊ⵗ̥̥̥̥̥̥̥̥̥̥̊̊ⵗ̥̥̥̥̥̥̥̥̥̥̥ⵗ̥̥̥̥̥̥̥̥̥̥̊̊ⵗ̥̥̥̥̥̥̥̥̥̊̊̊ⵗ̥̥̥̥̥̥̥̥̊̊̊̊ⵗ̥̥̥̥̥̥̥̊̊̊̊̊ⵗ̥̥̥̥̥̥̊̊̊̊̊ⵗ̥̥̥̥̥̊̊̊̊ⵗ̥̥̥̥̊̊̊ⵗ̥̥̊̊ \n\n🦋║ლ💞 💞 ლ║🦋\n\n💐☘️-ধন্যবাদ প্রিয়-☘️💐\n𝄞❤️⋆⃝⑅⑅⃝❤️»̶̶͓͓̽̽̽»̶̶͓͓̽̽̽.𝐁𝐎𝐓-𝐀𝐃𝐌𝐈𝐍♥🖤𝐑𝐀𝐉𝐀-𝐁𝐀𝐁𝐔❤️⃪⃝⃘᭄⃕❤️" : msg = threadData.customJoin;
-      msg = msg
-      .replace(/\{uName}/g, nameArray.join(', '))
-      .replace(/\{type}/g, (memLength.length > 1) ?  'you' : 'Friend')
-      .replace(/\{soThanhVien}/g, memLength.join(', '))
-      .replace(/\{threadName}/g, threadName);			
-
-      var link = [
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
-"https://i.imgur.com/i6bqJPo.mp4",
-      ];
-        var callback = () => api.sendMessage({ body: msg, attachment: fs.createReadStream(__dirname + "/cache/leiamnashJ.mp4"), mentions }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/leiamnashJ.mp4"));
-    return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/leiamnashJ.mp4")).on("close", () => callback());       
-            }
-})
-        }
-    }catch (err) {
-            return console.log("ERROR: "+err);
+    name: "joinNoti",
+    eventType: ["log:subscribe"],
+    version: "1.0.1",
+    credits: "CatalizCS",
+    description: "Notification of bots or people entering groups with random gif/photo/video",
+    dependencies: {
+        "fs-extra": "",
+        "path": "",
+        "pidusage": ""
     }
-  }
-        }
+};
+ 
+module.exports.onLoad = function () {
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { join } = global.nodemodule["path"];
+ 
+    const path = join(__dirname, "cache", "joinGif");
+    if (existsSync(path)) mkdirSync(path, { recursive: true }); 
+ 
+    const path2 = join(__dirname, "cache", "joinGif", "randomgif");
+    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
+ 
+    return;
+}
+ 
+ 
+module.exports.run = async function({ api, event }) {
+    const { join } = global.nodemodule["path"];
+    const { threadID } = event;
+    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+        api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? " " : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+        const fs = require("fs");
+        return api.sendMessage("চলে এসেছি আমি পিচ্চি রাৃঁকিৃঁবৃঁ চৌৃঁধুৃঁরীৃঁ তোমাদের মাঝে🫣🫂", event.threadID, () => api.sendMessage({body: `${global.config.BOTNAME} - Bot Connected ✅\n─────────────────\n╭────────────╮\n🌻Total users: ${global.data.allUserID.length}🌻\n╰────────────╯\n ─────────────────\n𝐀𝐬𝐬𝐚𝐥𝐚𝐦 𝐀𝐥𝐚𝐢𝐤𝐮𝐦\n─────────────────\n𝐌𝐲 𝐍𝐚𝐦𝐞 𝐈𝐬 ${global.config.BOTNAME} \n─────────────────\nMy Prefix Is [ ${global.config.PREFIX} ]\n─────────────────\nType ${global.config.PREFIX}help to see my cmd list\n─────────────────\nMy Owner Is RAKIB CHOWDHURY.\n─────────────────\nUse ${global.config.PREFIX}Called For Any Issues:\n─────────────────\n╭────────╮\n🌷::𝐄𝐱𝐚𝐦𝐩𝐥𝐞::🌷\n╰────────╯\n─────────────────\n /Text /Mark /Obama\n/Says /Music /Zuck \n/Lexi /Pair /Bot /Help\n ─────────────────`, attachment: fs.createReadStream(__dirname + "/cache/wel.mp4")} ,threadID));
+    }
+    else {
+        try {
+            const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+            let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+ 
+            const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+            const path = join(__dirname, "cache", "joinGif");
+            const pathGif = join(path, `${threadID}.gif`);
+ 
+            var mentions = [], nameArray = [], memLength = [], i = 0;
+            
+            for (id in event.logMessageData.addedParticipants) {
+                const userName = event.logMessageData.addedParticipants[id].fullName;
+                nameArray.push(userName);
+                mentions.push({ tag: userName, id });
+                memLength.push(participantIDs.length - i++);
+            }
+            memLength.sort((a, b) => a - b);
+            
+            (typeof threadData.customJoin == "undefined") ? msg = "۞̳̳̳̳̳̳̳̳̳̳̿̿࿅»̶̶͓͓͓̽̽̽»̶̶͓͓͓̽̽̽❥⃝ আসসালামু আলাইকুম🌺 🥀༊🤗😻🤗༊🥀۞̳̳̳̳̳̳̳̳̳̳̿̿࿅»̶̶͓͓͓̽̽̽»̶̶͓͓͓̽̽̽❥⃝-Wellcome-!!-🌻🥀\n\n {name}. আপনি এই গ্রুপের  {soThanhVien} নং মেম্বার\n\n {threadName}\n\nnগ্রুপে🍀🫰\n                                                                         🖤🦋🥀আসার জন্য তোমাকে অন্তরের অন্তর স্থল থেকে জানাই অবিরাম ভালবাসা!!-🖤🌻🥀🖤━━╬٨♪❤٨ـ \n                                                           ⭕⭕হা্ঁসি্ঁ ম্ঁজা্ঁ ঠা্ঁট্টা্ঁয়্ঁ ব্ঁন্ধু্ঁত্ব্ঁ হ্ঁবে্ঁ চি্ঁর্ঁকা্ঁলী্ঁন্ঁ বে্ঁচে্ঁ থা্ঁকু্ঁক্ঁ ব্ঁন্ধু্ঁত্ব্ঁ ভা্ঁলো্ঁবা্ঁসা্ঁর্ঁ স্ঁম্প্ঁর্কে্ঁ আ্ঁজী্ঁব্ঁন্ঁ⭕⭕\n                                 ❉্᭄͜͡...\n                                          ▓▓▓▓▓▓▓▓\n                                           .                                                                         আমাদের সাথে সময় দেওয়া ও পাশে থাকার অনুরোধ রইলো !!-🍂🌺🥀\n                                       🦋༎❤❤༎🦋\n\n🥰🥀ᏔᎬᏞᏟϴᎷᎬ 🥀🥰 \n\🦋║ლ💞🖤💞 ლ║🦋\n\n💐☘️-ধন্যবাদ প্রিয়--☘️💐─\n\n\𝄞❤️⋆⃝⑅⑅⃝❤️»̶̶͓͓̽̽̽»̶̶͓͓̽̽̽BOT OWNER: Rakib Chowdhury ❤️⃪⃝⃘᭄⃕❤️\n\n\𝄞❤️⋆⃝⑅⑅⃝❤️»̶̶͓͓̽̽̽»̶̶͓͓̽̽̽ID LINK= https://www.facebook.com/RAKIBCHOWDHURY.A?mibextid=ZbWKwL❤️⃪⃝⃘᭄⃕❤️─\n\n\𝄞❤️⋆⃝⑅⑅⃝❤️»̶̶͓͓̽̽̽»̶̶͓͓̽̽̽MASSANGER LINK:https://m.me/RAKIBCHOWDHURY.A❤️⃪⃝⃘᭄⃕❤️🥳😍" : msg = threadData.customJoin;
+            msg = msg
+            .replace(/\{name}/g, nameArray.join(', '))
+            .replace(/\{type}/g, (memLength.length > 1) ?  'Friends' : 'Friend')
+            .replace(/\{soThanhVien}/g, memLength.join(', '))
+            .replace(/\{threadName}/g, threadName);
+ 
+            if (existsSync(path)) mkdirSync(path, { recursive: true });
+ 
+            const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
+ 
+            if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+            else if (randomPath.length != 0) {
+                const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+                formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
+            }
+            else formPush = { body: msg, mentions }
+ 
+            return api.sendMessage(formPush, threadID);
+        } catch (e) { return console.log(e) };
+    }
+    }
